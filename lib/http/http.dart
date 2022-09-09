2e0d4824d1c4b37b430d5532bbd46b3ea29210f3
weimuhua <weimuhua0812@126.com>
2022-09-09 12:22:56 +0800

    feat: dio增加拦截器

--- lib/http/http.dart
+++ lib/http/http.dart
@@ -1,10 +1,22 @@
 import 'package:dio/dio.dart';
+import 'package:f_logs/f_logs.dart';
 
 class HttpUtil {
   static var dio = Dio();
 
   static void init() {
     dio.options.contentType = 'application/json';
+    dio.interceptors.add(InterceptorsWrapper(onRequest: (options, handler) {
+      FLog.info(text: "onRequest, url: ${options.path}");
+      return handler.next(options);
+    }, onResponse: (response, handler) {
+      FLog.info(text: "onResponse, code ${response.statusCode}, url: ${response.realUri}");
+      FLog.debug(text: 'onResponse：${response.data}');
+      handler.next(response);
+    }, onError: (DioError e, handler) {
+      FLog.error(text: 'http onError', exception: e);
+      return handler.next(e);
+    }));
   }
 
   static Future<Response> get(
@@ -14,9 +26,6 @@ class HttpUtil {
     CancelToken? cancelToken,
     ProgressCallback? onReceiveProgress,
   }) {
-    return dio.get(path,
-        queryParameters: queryParameters,
-        options: options,
-        cancelToken: cancelToken);
+    return dio.get(path, queryParameters: queryParameters, options: options, cancelToken: cancelToken);
   }
 }
